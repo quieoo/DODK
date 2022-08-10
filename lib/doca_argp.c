5d502f0aad5e78e0d1ce2702f38dbe5798931c29
quieoo <715094648@qq.com>
2022-08-10 15:42:25 +0800

    dpdk initialize

--- lib/doca_argp.c
+++ lib/doca_argp.c
@@ -3,11 +3,9 @@
 #include <rte_eal.h>
 
 void doca_argp_init(const char *program_name, struct doca_argp_program_type_config *type_config, void *program_config){
-    printf("``doca_argp_init called\n");
 }
 
 void doca_argp_register_param(struct doca_argp_param *input_param){
-    printf("``doca_argp_register_param called\n");
 }
 
 void doca_argp_start(int argc, char **argv, struct doca_argp_program_general_config **general_config){
--- lib/dpdk_utils.c
+++ lib/dpdk_utils.c
@@ -1,7 +1,189 @@
 #include "dpdk_utils.h"
+#include <rte_ethdev.h>
+
+static int
+port_init(struct rte_mempool *mbuf_pool, uint8_t port, struct application_dpdk_config *app_config)
+{
+	int ret;
+	uint16_t i;
+	/* Ethernet port configured with default settings. 8< */
+	struct rte_eth_conf port_conf = {
+		.rxmode = {
+			.split_hdr_size = 0,
+		},
+		.txmode = {
+			.offloads =
+				RTE_ETH_TX_OFFLOAD_VLAN_INSERT |
+				RTE_ETH_TX_OFFLOAD_IPV4_CKSUM  |
+				RTE_ETH_TX_OFFLOAD_UDP_CKSUM   |
+				RTE_ETH_TX_OFFLOAD_TCP_CKSUM   |
+				RTE_ETH_TX_OFFLOAD_SCTP_CKSUM  |
+				RTE_ETH_TX_OFFLOAD_TCP_TSO,
+		},
+	};
+	struct rte_eth_txconf txq_conf;
+	struct rte_eth_rxconf rxq_conf;
+	struct rte_eth_dev_info dev_info;
+
+	ret = rte_eth_dev_info_get(port, &dev_info);
+	if (ret != 0)
+		rte_exit(EXIT_FAILURE,
+			"Error during getting device (port %u) info: %s\n",
+			port, strerror(-ret));
+
+	port_conf.txmode.offloads &= dev_info.tx_offload_capa;
+	printf(":: initializing port: %d\n", port);
+	ret = rte_eth_dev_configure(port,
+				app_config->port_config.nb_queues, app_config->port_config.nb_queues, &port_conf);
+	if (ret < 0) {
+		rte_exit(EXIT_FAILURE,
+			":: cannot configure device: err=%d, port=%u\n",
+			ret, port);
+	}
+
+	rxq_conf = dev_info.default_rxconf;
+	rxq_conf.offloads = port_conf.rxmode.offloads;
+	/* >8 End of ethernet port configured with default settings. */
+
+	/* Configuring number of RX and TX queues connected to single port. 8< */
+	for (i = 0; i < app_config->port_config.nb_queues; i++) {
+		ret = rte_eth_rx_queue_setup(port, i, 512,
+				     rte_eth_dev_socket_id(port),
+				     &rxq_conf,
+				     mbuf_pool);
+		if (ret < 0) {
+			rte_exit(EXIT_FAILURE,
+				":: Rx queue setup failed: err=%d, port=%u\n",
+				ret, port);
+		}
+	}
+	txq_conf = dev_info.default_txconf;
+	txq_conf.offloads = port_conf.txmode.offloads;
+
+	for (i = 0; i < app_config->port_config.nb_queues; i++) {
+		ret = rte_eth_tx_queue_setup(port, i, 512,
+				rte_eth_dev_socket_id(port),
+				&txq_conf);
+		if (ret < 0) {
+			rte_exit(EXIT_FAILURE,
+				":: Tx queue setup failed: err=%d, port=%u\n",
+				ret, port);
+		}
+	}
+	/* >8 End of Configuring RX and TX queues connected to single port. */
+
+
+	/* Setting the RX port to promiscuous mode. 8< */
+	ret = rte_eth_promiscuous_enable(port);
+	if (ret != 0)
+		rte_exit(EXIT_FAILURE,
+			":: promiscuous mode enable failed: err=%s, port=%u\n",
+			rte_strerror(-ret), port);
+	/* >8 End of setting the RX port to promiscuous mode. */
+
+	/* Starting the port. 8< */
+	ret = rte_eth_dev_start(port);
+	if (ret < 0) {
+		rte_exit(EXIT_FAILURE,
+			"rte_eth_dev_start:err=%d, port=%u\n",
+			ret, port);
+	}
+	/* >8 End of starting the port. */
+
+	assert_link_status(port);
+
+	printf(":: initializing port: %d done\n", port);
+}
+
+static int
+dpdk_ports_init(struct application_dpdk_config *app_config)
+{
+    int ret;
+	uint8_t port_id;
+	const uint8_t nb_ports = app_config->port_config.nb_ports;
+	const uint32_t total_nb_mbufs = app_config->port_config.nb_queues * nb_ports * NUM_MBUFS;
+	struct rte_mempool *mbuf_pool;    
+
+    /* Initialize mbufs mempool */
+    mbuf_pool = allocate_mempool(total_nb_mbufs);
+
+    ret = rte_flow_dynf_metadata_register();
+	if (ret < 0)
+		APP_EXIT("Metadata register failed");
+    for (port_id = 0; port_id < nb_ports; port_id++)
+		if (port_init(mbuf_pool, port_id, app_config) != 0)
+			APP_EXIT("Cannot init port %" PRIu8, port_id);
+	return ret;
+}
+
+static int
+bind_hairpin_queues(uint16_t port_id)
+{
+	/* Configure the Rx and Tx hairpin queues for the selected port. */
+	int ret = 0, peer_port, peer_ports_len;
+	uint16_t peer_ports[RTE_MAX_ETHPORTS];
+
+	/* bind current Tx to all peer Rx */
+	peer_ports_len = rte_eth_hairpin_get_peer_ports(port_id, peer_ports, RTE_MAX_ETHPORTS, 1);
+	if (peer_ports_len < 0)
+		return peer_ports_len;
+	for (peer_port = 0; peer_port < peer_ports_len; peer_port++) {
+		ret = rte_eth_hairpin_bind(port_id, peer_ports[peer_port]);
+		if (ret < 0)
+			return ret;
+	}
+	/* bind all peer Tx to current Rx */
+	peer_ports_len = rte_eth_hairpin_get_peer_ports(port_id, peer_ports, RTE_MAX_ETHPORTS, 0);
+	if (peer_ports_len < 0)
+		return peer_ports_len;
+	for (peer_port = 0; peer_port < peer_ports_len; peer_port++) {
+		ret = rte_eth_hairpin_bind(peer_ports[peer_port], port_id);
+		if (ret < 0)
+			return ret;
+	}
+	return ret;
+}
+
+static void
+enable_hairpin_queues(uint8_t nb_ports)
+{
+	uint8_t port_id;
+
+	for (port_id = 0; port_id < nb_ports; port_id++)
+		if (bind_hairpin_queues(port_id) != 0)
+			APP_EXIT("Hairpin bind failed on port=%u", port_id);
+}
 
 void dpdk_init(struct application_dpdk_config *app_dpdk_config){
+    int ret = 0;
 
+    /* Check that DPDK enabled the required ports to send/receive on */
+
+    ret = rte_eth_dev_count_avail();
+	if (app_dpdk_config->port_config.nb_ports > 0 && ret != app_dpdk_config->port_config.nb_ports)
+		APP_EXIT("Application will only function with %u ports, num_of_ports=%d",app_dpdk_config->port_config.nb_ports, ret);
+    
+    /* Check for available logical cores */
+    ret = rte_lcore_count();
+	if (app_dpdk_config->port_config.nb_queues > 0 && ret < app_dpdk_config->port_config.nb_queues)
+		APP_EXIT("At least %d cores are needed for the application to run, available_cores=%d", app_dpdk_config->port_config.nb_queues, ret);
+	else
+		app_dpdk_config->port_config.nb_queues = ret;
+    
+    if (app_dpdk_config->reserve_main_thread)
+		app_dpdk_config->port_config.nb_queues -= 1;
+
+    if (app_dpdk_config->port_config.nb_ports > 0 && dpdk_ports_init(app_dpdk_config) != 0)
+		APP_EXIT("Ports allocation failed");
+
+	/* Enable hairpin queues */
+	if (app_dpdk_config->port_config.nb_hairpin_q > 0)
+		enable_hairpin_queues(app_dpdk_config->port_config.nb_ports);
+
+	/*	
+	if (app_dpdk_config->sft_config.enable)
+		dpdk_sft_init(app_dpdk_config);
+	*/
 }
 
 void dpdk_fini(struct application_dpdk_config *app_dpdk_config){
