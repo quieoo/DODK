663f43bd352bf42de61009f49f96d1d46faef289
quieoo <715094648@qq.com>
2022-08-12 16:16:04 +0800

    t

--- lib/doca_log.c
+++ lib/doca_log.c
@@ -23,6 +23,7 @@ struct doca_logger_backend *doca_log_create_buffer_backend(char *buffer, size_t 
 struct doca_logger_backend *doca_log_create_syslog_backend(const char *name){}
 void doca_log(uint32_t level, uint32_t source, int line, const char *format, ...)
 {
+    printf("LOG-%d: ",level);
     printf(format);
     printf("\n");
 }
