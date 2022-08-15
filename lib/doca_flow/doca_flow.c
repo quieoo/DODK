b7b1b107af4f7c76e1dc63b157a5835e76d33817
quieoo <715094648@qq.com>
2022-08-15 17:34:44 +0800

    t

--- lib/doca_flow/doca_flow.c
+++ lib/doca_flow/doca_flow.c
@@ -73,7 +73,7 @@ doca_flow_create_pipe(const struct doca_flow_pipe_cfg *cfg,
 	pipe->port_id = cfg->port->port_id;
 	return pipe;
 }
-print_ether_addr(const char *what, uint8_t *eth_addr)
+print_ether_addr(const char *what, uint8_t eth_addr[])
 {
 	printf("%d\n",sizeof(eth_addr));
 	printf("%d\n",*eth_addr);
