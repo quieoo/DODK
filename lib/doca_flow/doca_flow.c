71bcacc4e91a2e29cfa2de7c3208f26bf5249ce1
quieoo <715094648@qq.com>
2022-08-15 16:56:54 +0800

    outputflow

--- lib/doca_flow/doca_flow.c
+++ lib/doca_flow/doca_flow.c
@@ -10,203 +10,213 @@
 #include <rte_net.h>
 #include <rte_flow.h>
 
-#define MAX_PATTERN_NUM		10
-#define MAX_ACTION_NUM		10
+#define MAX_PATTERN_NUM 10
+#define MAX_ACTION_NUM 10
 
-int
-doca_flow_init(const struct doca_flow_cfg *cfg,
-	       struct doca_flow_error *error)
+int doca_flow_init(const struct doca_flow_cfg *cfg,
+				   struct doca_flow_error *error)
 {
 	// printf("doca_flow_init\n"); // check
 	return 0;
 }
 
-void
-doca_flow_destroy(void){}
+void doca_flow_destroy(void) {}
 
-typedef struct doca_flow_port 
+typedef struct doca_flow_port
 {
-  int port_id; 
+	int port_id;
 };
 
 struct doca_flow_port ports[10];
 
 struct doca_flow_port *
 doca_flow_port_start(const struct doca_flow_port_cfg *cfg,
-		     struct doca_flow_error *error)
+					 struct doca_flow_error *error)
 {
-	int id=atoi(cfg->devargs);
+	int id = atoi(cfg->devargs);
 
-	struct doca_flow_port *port=malloc(sizeof(struct doca_flow_port));
-	port->port_id=id;
+	struct doca_flow_port *port = malloc(sizeof(struct doca_flow_port));
+	port->port_id = id;
 
-	ports[id]=*port;
+	ports[id] = *port;
 
 	return port;
 }
 
-int
-doca_flow_port_stop(struct doca_flow_port *port){}
+int doca_flow_port_stop(struct doca_flow_port *port) {}
 
-int
-doca_flow_port_pair(struct doca_flow_port *port, struct doca_flow_port *pair_port){}
+int doca_flow_port_pair(struct doca_flow_port *port, struct doca_flow_port *pair_port) {}
 
-uint8_t*
-doca_flow_port_priv_data(struct doca_flow_port *port){}
-int
-doca_flow_shared_resource_cfg(enum doca_flow_shared_resource_type type, uint32_t id,
-			      struct doca_flow_shared_resource_cfg *cfg,
-			      struct doca_flow_error *error){}
+uint8_t *
+doca_flow_port_priv_data(struct doca_flow_port *port) {}
+int doca_flow_shared_resource_cfg(enum doca_flow_shared_resource_type type, uint32_t id,
+								  struct doca_flow_shared_resource_cfg *cfg,
+								  struct doca_flow_error *error) {}
 
-int
-doca_flow_shared_resources_bind(enum doca_flow_shared_resource_type type, uint32_t *res_array,
-				uint32_t res_array_len, void *bindable_obj,
-				struct doca_flow_error *error){}
+int doca_flow_shared_resources_bind(enum doca_flow_shared_resource_type type, uint32_t *res_array,
+									uint32_t res_array_len, void *bindable_obj,
+									struct doca_flow_error *error) {}
 
-
-typedef struct doca_flow_pipe{
+typedef struct doca_flow_pipe
+{
 	int port_id;
 };
 
 struct doca_flow_pipe *
 doca_flow_create_pipe(const struct doca_flow_pipe_cfg *cfg,
-		const struct doca_flow_fwd *fwd,
-		const struct doca_flow_fwd *fwd_miss,
-		struct doca_flow_error *error)
+					  const struct doca_flow_fwd *fwd,
+					  const struct doca_flow_fwd *fwd_miss,
+					  struct doca_flow_error *error)
 {
-	struct doca_flow_pipe *pipe=malloc(sizeof(struct doca_flow_pipe));
+	struct doca_flow_pipe *pipe = malloc(sizeof(struct doca_flow_pipe));
 
-	pipe->port_id=cfg->port->port_id;
+	pipe->port_id = cfg->port->port_id;
 	return pipe;
 }
 
-void output_flow(uint16_t port_id, const struct rte_flow_attr *attr, const struct rte_flow_item *pattern, const struct rte_flow_action *actions, struct rte_flow_error *error){
+void output_flow(uint16_t port_id, const struct rte_flow_attr *attr, const struct rte_flow_item *pattern, const struct rte_flow_action *actions, struct rte_flow_error *error)
+{
 	printf("{\n");
-	
-	printf("	port_id: %d\n",port_id);
-	
+
+	printf("	port_id: %d\n", port_id);
+
 	printf("	attr: \n");
-	printf("		egress: %d\n",attr->egress);
-	printf("		group: %d\n",attr->group);
-	printf("		ingress: %d\n",attr->ingress);
-	printf("		priority: %d\n",attr->priority);
-	printf("		transfer: %d\n",attr->transfer);
-	int i=0;
-	
-	for (; pattern->type != RTE_FLOW_ITEM_TYPE_END; pattern++){
-		printf("	pattern-%d:\n",i++);
+	printf("		egress: %d\n", attr->egress);
+	printf("		group: %d\n", attr->group);
+	printf("		ingress: %d\n", attr->ingress);
+	printf("		priority: %d\n", attr->priority);
+	printf("		transfer: %d\n", attr->transfer);
+	int i = 0;
+
+	for (; pattern->type != RTE_FLOW_ITEM_TYPE_END; pattern++)
+	{
+		printf("	pattern-%d:\n", i++);
 		printf("		type: ");
 		switch (pattern->type)
 		{
-			case RTE_FLOW_ITEM_TYPE_VOID:
-				printf("RTE_FLOW_ITEM_TYPE_VOID\n");
-				break;
-			case RTE_FLOW_ITEM_TYPE_ETH:
-				printf("RTE_FLOW_ITEM_TYPE_ETH\n");
-				break;
-			case RTE_FLOW_ITEM_TYPE_IPV4:
-				printf("RTE_FLOW_ITEM_TYPE_IPV4\n");
-				const struct rte_flow_item_ipv4 *mask = pattern->mask;
-				struct in_addr mask_dst,mask_src;
-				if(mask!=NULL){
-					mask_dst.s_addr=mask->hdr.dst_addr;
-					mask_src.s_addr=mask->hdr.src_addr;
-					printf("		mask.hdr:\n");
-					printf("			src_addr:%s\n",inet_ntoa(mask_src));
-					printf("			dst_addr: %s\n",inet_ntoa(mask_dst));
-				}
-				const struct rte_flow_item_ipv4 *spec = pattern->spec;
-				struct in_addr dst,src;
-				dst.s_addr=spec->hdr.dst_addr;
-				src.s_addr=spec->hdr.src_addr;
+		case RTE_FLOW_ITEM_TYPE_VOID:
+			printf("RTE_FLOW_ITEM_TYPE_VOID\n");
+			break;
+		case RTE_FLOW_ITEM_TYPE_ETH:
+			printf("RTE_FLOW_ITEM_TYPE_ETH\n");
+			break;
+		case RTE_FLOW_ITEM_TYPE_IPV4:
+		{
+			printf("RTE_FLOW_ITEM_TYPE_IPV4\n");
+			const struct rte_flow_item_ipv4 *mask = pattern->mask;
+			struct in_addr mask_dst, mask_src;
+			if (mask != NULL)
+			{
+				mask_dst.s_addr = mask->hdr.dst_addr;
+				mask_src.s_addr = mask->hdr.src_addr;
+				printf("		mask.hdr:\n");
+				printf("			src_addr:%s\n", inet_ntoa(mask_src));
+				printf("			dst_addr: %s\n", inet_ntoa(mask_dst));
+			}
+			const struct rte_flow_item_ipv4 *spec = pattern->spec;
+			struct in_addr dst, src;
+			dst.s_addr = spec->hdr.dst_addr;
+			src.s_addr = spec->hdr.src_addr;
 
-				printf("		spec.hdr:\n");
-				printf("			src_addr:%s\n",inet_ntoa(src));
-				printf("			dst_addr:%s\n",inet_ntoa(dst));
-				
-				break;
-			case RTE_FLOW_ITEM_TYPE_UDP:
-				printf("RTE_FLOW_ITEM_TYPE_UDP\n");
-				const struct rte_flow_item_udp *udpspec = pattern->spec;
-				
-				dst.s_addr=udpspec->hdr.dst_port;
-				src.s_addr=udpspec->hdr.src_port;
+			printf("		spec.hdr:\n");
+			printf("			src_addr:%s\n", inet_ntoa(src));
+			printf("			dst_addr:%s\n", inet_ntoa(dst));
 
-				printf("		spec.hdr:\n");
-				printf("			src_addr:%s\n",inet_ntoa(src));
-				printf("			dst_addr:%s\n",inet_ntoa(dst));
-				
-				break;
-			default:
-				printf("other type: %d\n",pattern->type);
+			break;
 		}
+		case RTE_FLOW_ITEM_TYPE_UDP:
+		{
+			printf("RTE_FLOW_ITEM_TYPE_UDP\n");
+			const struct rte_flow_item_udp *udpspec = pattern->spec;
+			struct in_addr dst, src;
+			dst.s_addr = udpspec->hdr.dst_port;
+			src.s_addr = udpspec->hdr.src_port;
 
+			printf("		spec.hdr:\n");
+			printf("			src_addr:%s\n", inet_ntoa(src));
+			printf("			dst_addr:%s\n", inet_ntoa(dst));
+
+			break;
+		}
+		default:
+			printf("other type: %d\n", pattern->type);
+		}
 	}
-	i=0;
-	for (; actions->type != RTE_FLOW_ACTION_TYPE_END; actions++) {
-		printf("	action-%d:\n",i++);
+	i = 0;
+	for (; actions->type != RTE_FLOW_ACTION_TYPE_END; actions++)
+	{
+		printf("	action-%d:\n", i++);
 		printf("		type: ");
 
 		switch (actions->type)
 		{
-			case RTE_FLOW_ACTION_TYPE_VOID:
-				printf("RTE_FLOW_ACTION_TYPE_VOID\n");
-				break;
-			case RTE_FLOW_ACTION_TYPE_DROP:
-				printf("RTE_FLOW_ACTION_TYPE_DROP\n");
-				break;
-			case RTE_FLOW_ACTION_TYPE_QUEUE:
-				printf("RTE_FLOW_ACTION_TYPE_QUEUE\n");
-				const struct rte_flow_action_queue *queue = actions->conf;
-				printf("		index:%d\n",queue->index);
-				break;
-			case RTE_FLOW_ACTION_TYPE_SET_MAC_DST:
-				printf("RTE_FLOW_ACTION_TYPE_SET_MAC_DST\n");
-				const struct rte_flow_action_set_mac *dst_mac=actions->conf;
-				printf("		mac_addr: ");
-				for(int i=0;i<RTE_ETHER_ADDR_LEN;i++){
-					printf("%x",dst_mac[i]);
-				}
-				printf("\n");
-				break;
-			case RTE_FLOW_ACTION_TYPE_SET_IPV4_DST:
-				printf("RTE_FLOW_ACTION_TYPE_SET_IPV4_DST\n");
-				const struct rte_flow_action_set_ipv4 *dst_ip=actions->conf;
-				struct in_addr addr;
-				addr.s_addr=dst_ip->ipv4_addr;
-				printf("		dst_addr:%s\n",inet_ntoa(addr));
-				break;
-			case RTE_FLOW_ACTION_TYPE_SET_TP_DST:
-				const struct rte_flow_action_set_tp *dst_tp=actions->conf;
-				printf("		port: %d\n",dst_tp);
-				break;
-			case RTE_FLOW_ACTION_TYPE_PORT_ID:
-				const struct rte_flow_action_port_id *pid=actions->conf;
-				printf("		port_id: %d\n",pid->id);
-				break;
-			
-			default:
-				printf("other type: %d\n",actions->type);
+		case RTE_FLOW_ACTION_TYPE_VOID:
+			printf("RTE_FLOW_ACTION_TYPE_VOID\n");
+			break;
+		case RTE_FLOW_ACTION_TYPE_DROP:
+			printf("RTE_FLOW_ACTION_TYPE_DROP\n");
+			break;
+		case RTE_FLOW_ACTION_TYPE_QUEUE:
+		{
+			printf("RTE_FLOW_ACTION_TYPE_QUEUE\n");
+			const struct rte_flow_action_queue *queue = actions->conf;
+			printf("		index:%d\n", queue->index);
+			break;
+		}
+		case RTE_FLOW_ACTION_TYPE_SET_MAC_DST:
+		{
+			printf("RTE_FLOW_ACTION_TYPE_SET_MAC_DST\n");
+			const struct rte_flow_action_set_mac *dst_mac = actions->conf;
+			printf("		mac_addr: ");
+			for (int i = 0; i < RTE_ETHER_ADDR_LEN; i++)
+			{
+				printf("%x", dst_mac[i]);
+			}
+			printf("\n");
+			break;
+		}
+		case RTE_FLOW_ACTION_TYPE_SET_IPV4_DST:
+		{
+			printf("RTE_FLOW_ACTION_TYPE_SET_IPV4_DST\n");
+			const struct rte_flow_action_set_ipv4 *dst_ip = actions->conf;
+			struct in_addr addr;
+			addr.s_addr = dst_ip->ipv4_addr;
+			printf("		dst_addr:%s\n", inet_ntoa(addr));
+			break;
+		}
+		case RTE_FLOW_ACTION_TYPE_SET_TP_DST:
+		{
+			const struct rte_flow_action_set_tp *dst_tp = actions->conf;
+			printf("		port: %d\n", dst_tp);
+			break;
+		}
+		case RTE_FLOW_ACTION_TYPE_PORT_ID:
+		{
+			const struct rte_flow_action_port_id *pid = actions->conf;
+			printf("		port_id: %d\n", pid->id);
+			break;
 		}
 
+		default:
+			printf("other type: %d\n", actions->type);
+		}
 	}
-	
+
 	printf("}\n");
-	
 }
 
-
-struct doca_flow_pipe_entry*
-doca_flow_pipe_add_entry(uint16_t pipe_queue, 
-struct doca_flow_pipe *pipe, 
-const struct doca_flow_match *match, 
-const struct doca_flow_actions *actions, 
-const struct doca_flow_monitor *monitor, 
-const struct doca_flow_fwd *fwd, 
-uint32_t flags, 
-void *usr_ctx, 
-struct doca_flow_error *error){
-	//dpdk need structures
+struct doca_flow_pipe_entry *
+doca_flow_pipe_add_entry(uint16_t pipe_queue,
+						 struct doca_flow_pipe *pipe,
+						 const struct doca_flow_match *match,
+						 const struct doca_flow_actions *actions,
+						 const struct doca_flow_monitor *monitor,
+						 const struct doca_flow_fwd *fwd,
+						 uint32_t flags,
+						 void *usr_ctx,
+						 struct doca_flow_error *error)
+{
+	// dpdk need structures
 	struct rte_flow_attr attr;
 	struct rte_flow_item pattern[MAX_PATTERN_NUM];
 	struct rte_flow_action action[MAX_ACTION_NUM];
@@ -215,69 +225,70 @@ struct doca_flow_error *error){
 	memset(action, 0, sizeof(action));
 	memset(&attr, 0, sizeof(struct rte_flow_attr));
 
-	attr.ingress=1;
+	attr.ingress = 1;
 
 	/*convert match->pattern*/
-	//mac
-	int p=0;
-	pattern[p].type=RTE_FLOW_ITEM_TYPE_ETH;
+	// mac
+	int p = 0;
+	pattern[p].type = RTE_FLOW_ITEM_TYPE_ETH;
 	struct rte_flow_item_eth mac_spec;
-	memset(&mac_spec,0,sizeof(struct rte_flow_item_eth));
-	memcpy(mac_spec.hdr.dst_addr.addr_bytes, match->out_dst_mac,DOCA_ETHER_ADDR_LEN);
-	memcpy(mac_spec.hdr.src_addr.addr_bytes, match->out_src_mac,DOCA_ETHER_ADDR_LEN);
-	pattern[p++].spec=&mac_spec;
-	
-	//ip
-	pattern[p].type=RTE_FLOW_ITEM_TYPE_IPV4;
+	memset(&mac_spec, 0, sizeof(struct rte_flow_item_eth));
+	memcpy(mac_spec.hdr.dst_addr.addr_bytes, match->out_dst_mac, DOCA_ETHER_ADDR_LEN);
+	memcpy(mac_spec.hdr.src_addr.addr_bytes, match->out_src_mac, DOCA_ETHER_ADDR_LEN);
+	pattern[p++].spec = &mac_spec;
+
+	// ip
+	pattern[p].type = RTE_FLOW_ITEM_TYPE_IPV4;
 	struct rte_flow_item_ipv4 ip_spec;
 	memset(&ip_spec, 0, sizeof(struct rte_flow_item_ipv4));
-	ip_spec.hdr.src_addr=match->out_dst_ip.ipv4_addr;
-	ip_spec.hdr.dst_addr=match->out_src_ip.ipv4_addr;
-	pattern[p++].spec=&ip_spec;
-	
-	//TCP/UDP
+	ip_spec.hdr.src_addr = match->out_dst_ip.ipv4_addr;
+	ip_spec.hdr.dst_addr = match->out_src_ip.ipv4_addr;
+	pattern[p++].spec = &ip_spec;
+
+	// TCP/UDP
 	switch (match->out_l4_type)
 	{
 	case IPPROTO_UDP:
-		pattern[p].type=RTE_FLOW_ITEM_TYPE_UDP;
+		pattern[p].type = RTE_FLOW_ITEM_TYPE_UDP;
 		struct rte_flow_item_udp udp_spec;
-		memset(&udp_spec,0,sizeof(struct rte_flow_item_udp));
-		udp_spec.hdr.src_port=match->out_src_port;
-		udp_spec.hdr.dst_port=match->out_dst_port;
-		pattern[p++].spec=&udp_spec;
+		memset(&udp_spec, 0, sizeof(struct rte_flow_item_udp));
+		udp_spec.hdr.src_port = match->out_src_port;
+		udp_spec.hdr.dst_port = match->out_dst_port;
+		pattern[p++].spec = &udp_spec;
 		break;
-	
+
 	default:
-		printf("FLOW with L4 type: %d\n",match->out_l4_type);
+		printf("FLOW with L4 type: %d\n", match->out_l4_type);
 		break;
 	}
-	pattern[p].type=RTE_FLOW_ITEM_TYPE_END;
+	pattern[p].type = RTE_FLOW_ITEM_TYPE_END;
 
 	/*convert actions -> action*/
 	// modify packets
-	p=0;
-	action[p].type=RTE_FLOW_ACTION_TYPE_SET_MAC_DST;
+	p = 0;
+	action[p].type = RTE_FLOW_ACTION_TYPE_SET_MAC_DST;
 	struct rte_flow_action_set_mac dst_mac;
-	for(int i=0;i<6;i++){
-		dst_mac.mac_addr[i]=actions->mod_dst_mac[i];
+	for (int i = 0; i < 6; i++)
+	{
+		dst_mac.mac_addr[i] = actions->mod_dst_mac[i];
 	}
-	action[p++].conf=&dst_mac;
+	action[p++].conf = &dst_mac;
 
-	action[p].type=RTE_FLOW_ACTION_TYPE_SET_IPV4_DST;
+	action[p].type = RTE_FLOW_ACTION_TYPE_SET_IPV4_DST;
 	struct rte_flow_action_set_ipv4 set_ipv4;
-	set_ipv4.ipv4_addr=actions->mod_dst_ip.ipv4_addr;
-	action[p++].conf=&set_ipv4;
+	set_ipv4.ipv4_addr = actions->mod_dst_ip.ipv4_addr;
+	action[p++].conf = &set_ipv4;
 
-	action[p].type=RTE_FLOW_ACTION_TYPE_SET_TP_DST;
+	action[p].type = RTE_FLOW_ACTION_TYPE_SET_TP_DST;
 	struct rte_flow_action_set_tp set_tp;
-	set_tp.port=actions->mod_dst_port;
-	action[p++].conf=&set_tp;
+	set_tp.port = actions->mod_dst_port;
+	action[p++].conf = &set_tp;
 
 	// forward actions
 	switch (fwd->type)
 	{
 	case 1:
-		//DOCA_FLOW_FWD_RSS
+		// DOCA_FLOW_FWD_RSS
 		/*
 		action[p].type=RTE_FLOW_ACTION_TYPE_RSS;
 		struct rte_flow_action_rss _rss;
@@ -287,103 +298,91 @@ struct doca_flow_error *error){
 		*/
 		break;
 	case 2:
-		//DOCA_FLOW_FWD_PORT
-		action[p].type=RTE_FLOW_ACTION_TYPE_PORT_ID;
+		// DOCA_FLOW_FWD_PORT
+		action[p].type = RTE_FLOW_ACTION_TYPE_PORT_ID;
 		struct rte_flow_action_port_id _pid;
-		_pid.id=fwd->port_id;
-		action[p++].conf=&_pid;
+		_pid.id = fwd->port_id;
+		action[p++].conf = &_pid;
 		break;
 	case 3:
-		//DOCA_FLOW_FWD_PIPE
+		// DOCA_FLOW_FWD_PIPE
 		printf("DOCA FWD PIPE\n");
 		break;
 	case 4:
-		//DOCA_FLOW_FWD_DROP
-		action[p++].type=RTE_FLOW_ACTION_TYPE_DROP;
+		// DOCA_FLOW_FWD_DROP
+		action[p++].type = RTE_FLOW_ACTION_TYPE_DROP;
 		break;
 	default:
-		printf("DOCA FWD OTHER TYPE: %d\n",fwd->type);
+		printf("DOCA FWD OTHER TYPE: %d\n", fwd->type);
 		break;
 	}
 
-	struct rte_flow_action_queue queue = { .index = pipe_queue };
+	struct rte_flow_action_queue queue = {.index = pipe_queue};
 	action[p].type = RTE_FLOW_ACTION_TYPE_QUEUE;
 	action[p++].conf = &queue;
 
+	action[p].type = RTE_FLOW_ACTION_TYPE_END;
 
-	action[p].type=RTE_FLOW_ACTION_TYPE_END;
+	// get port id
+	int port_id = pipe->port_id;
 
-	
-	//get port id
-	int port_id=pipe->port_id;
-	
-	//validate and create entry
+	// validate and create entry
 	struct rte_flow_error rte_error;
-	int res=rte_flow_validate(port_id, &attr,pattern,action,&rte_error);
-	if(!res){
+	int res = rte_flow_validate(port_id, &attr, pattern, action, &rte_error);
+	if (!res)
+	{
 		flow = rte_flow_create(port_id, &attr, pattern, action, &rte_error);
-		if (!flow) {
+		if (!flow)
+		{
 			printf("Flow can't be created %d message: %s\n",
-				rte_error.type,
-				rte_error.message ? rte_error.message : "(no stated reason)");
+				   rte_error.type,
+				   rte_error.message ? rte_error.message : "(no stated reason)");
 			rte_exit(EXIT_FAILURE, "error in creating flow");
 		}
 		output_flow(port_id, &attr, pattern, action, &error);
-	}else{
-		printf("ERROR while validate flow: %d\n",res);
-		printf("%s\n",rte_error.message);
 	}
-
-
-
+	else
+	{
+		printf("ERROR while validate flow: %d\n", res);
+		printf("%s\n", rte_error.message);
+	}
 }
 
-struct doca_flow_pipe_entry*
+struct doca_flow_pipe_entry *
 doca_flow_control_pipe_add_entry(uint16_t pipe_queue,
-			uint8_t priority,
-			struct doca_flow_pipe *pipe,
-			const struct doca_flow_match *match,
-			const struct doca_flow_match *match_mask,
-			const struct doca_flow_fwd *fwd,
-			struct doca_flow_error *error){}
+								 uint8_t priority,
+								 struct doca_flow_pipe *pipe,
+								 const struct doca_flow_match *match,
+								 const struct doca_flow_match *match_mask,
+								 const struct doca_flow_fwd *fwd,
+								 struct doca_flow_error *error) {}
 
-int
-doca_flow_pipe_rm_entry(uint16_t pipe_queue, void *usr_ctx,
-			struct doca_flow_pipe_entry *entry){}
+int doca_flow_pipe_rm_entry(uint16_t pipe_queue, void *usr_ctx,
+							struct doca_flow_pipe_entry *entry) {}
 
-void
-doca_flow_destroy_pipe(uint16_t port_id,
-		       struct doca_flow_pipe *pipe){}
+void doca_flow_destroy_pipe(uint16_t port_id,
+							struct doca_flow_pipe *pipe) {}
 
+void doca_flow_port_pipes_flush(uint16_t port_id) {}
 
-void
-doca_flow_port_pipes_flush(uint16_t port_id){}
-
-void
-doca_flow_destroy_port(uint16_t port_id)
+void doca_flow_destroy_port(uint16_t port_id)
 {
-	
 }
 
-void
-doca_flow_port_pipes_dump(uint16_t port_id, FILE *f){}
+void doca_flow_port_pipes_dump(uint16_t port_id, FILE *f) {}
 
-int
-doca_flow_query(struct doca_flow_pipe_entry *entry,
-		struct doca_flow_query *query_stats){}
+int doca_flow_query(struct doca_flow_pipe_entry *entry,
+					struct doca_flow_query *query_stats) {}
 
-int
-doca_flow_handle_aging(struct doca_flow_port *port, uint16_t queue,
-			uint64_t quota, struct doca_flow_aged_query *entries, int len)
+int doca_flow_handle_aging(struct doca_flow_port *port, uint16_t queue,
+						   uint64_t quota, struct doca_flow_aged_query *entries, int len)
 {
 	// process no aging
 	return 0;
 }
-int
-doca_flow_entries_process(struct doca_flow_port *port,
-		uint16_t pipe_queue, uint64_t timeout,
-		uint32_t max_processed_entries){}
+int doca_flow_entries_process(struct doca_flow_port *port,
+							  uint16_t pipe_queue, uint64_t timeout,
+							  uint32_t max_processed_entries) {}
 
 enum doca_flow_entry_status
-doca_flow_entry_get_status(struct doca_flow_pipe_entry *entry){}
-
+doca_flow_entry_get_status(struct doca_flow_pipe_entry *entry) {}
