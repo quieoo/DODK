9f712185f5c61bb48584a0b61f82227f9810d943
quieoo <715094648@qq.com>
2022-08-16 10:40:10 +0800

    t

--- app/simple_fwd_vnf/simple_fwd.c
+++ app/simple_fwd_vnf/simple_fwd.c
@@ -199,8 +199,7 @@ simple_fwd_build_port_fwd_miss(struct simple_fwd_port_cfg *port_cfg,
 	uint16_t *queues = NULL;
 	int n_queues;
 
-	uint8_t out_dst_mac0[DOCA_ETHER_ADDR_LEN]={1};
-	printf("%d\n",sizeof(out_dst_mac0));
+	uint8_t out_dst_mac0[DOCA_ETHER_ADDR_LEN];
 	if((memcmp(match.out_dst_mac,out_dst_mac0,sizeof(out_dst_mac0)))==0){
 		printf("initializeddddd\n");
 		printf("check");
