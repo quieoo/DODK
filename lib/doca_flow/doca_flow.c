621f0e1525b6b9fc79cc8004afb6bb8620aaa57c
quieoo <715094648@qq.com>
2022-08-20 10:17:14 +0800

    t

--- lib/doca_flow/doca_flow.c
+++ lib/doca_flow/doca_flow.c
@@ -104,13 +104,13 @@ doca_flow_create_pipe(const struct doca_flow_pipe_cfg *cfg,
 					  const struct doca_flow_fwd *fwd_miss,
 					  struct doca_flow_error *error)
 {
-	printf("pipe %s, fwd ");
+	printf("pipe %s, fwd ",cfg->name);
 	if(fwd==NULL) printf("NULL");
 	else printf("%d",fwd->type);
 	printf(" fwd_miss ");
 	if(fwd_miss==NULL) printf("NULL");
 	else printf("%d",fwd_miss->type);
-	
+	printf("\n");
 
 	struct doca_flow_pipe *pipe = malloc(sizeof(struct doca_flow_pipe));
 	pipe->cfg = malloc(sizeof(struct doca_flow_pipe_cfg));
