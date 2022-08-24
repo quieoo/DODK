e22bca7c52100ac1327a4c39e423993224741915
quieoo <715094648@qq.com>
2022-08-24 21:04:56 +0800

    t

--- lib/doca_log/doca_log.c
+++ lib/doca_log/doca_log.c
@@ -56,9 +56,7 @@ void doca_log(uint32_t level, uint32_t source, int line, const char *format, ...
 	int ret;
 
 	va_start(ap, format);
-	ret = rte_vvlog(level, source, format, ap);
-
-
+	ret = rte_vlog(level, source, format, ap);
 	va_end(ap);
 
 //    rte_log(level, source, format);
