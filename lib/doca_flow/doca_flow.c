6e4867963c23459172acb0ca28990da67d9dd75a
quieoo <715094648@qq.com>
2022-08-25 10:46:00 +0800

    t

--- lib/doca_flow/doca_flow.c
+++ lib/doca_flow/doca_flow.c
@@ -720,7 +720,7 @@ doca_flow_pipe_add_entry(uint16_t pipe_queue,
 
 
 		struct in_addr addr;
-		addr.s_addr = item_ipv4->hdr.src_addr;
+		addr.s_addr = item_ipv4.hdr.src_addr;
 		printf("	%s\n", inet_ntoa(addr));
 	}
 
