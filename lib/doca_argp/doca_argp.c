8402624b68b4db349d7898b7f3650dbc32097048
quieoo <715094648@qq.com>
2022-08-26 21:42:11 +0800

    t

--- lib/doca_argp/doca_argp.c
+++ lib/doca_argp/doca_argp.c
@@ -130,6 +130,7 @@ void doca_argp_start(int argc, char **argv, struct doca_argp_program_general_con
 	shortopt[shortopt_point]='\0';
 	while ((opt = getopt_long(argc, argv, shortopt, lgopts, NULL)) != -1)
 	{
+		printf("opt: %c\n", opt);
 		bool hit_noce=false;
 		for(int i=0;i<registered;i++){
 			struct doca_argp_param *p=registered_param[i];
