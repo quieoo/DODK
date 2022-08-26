b8d5b9733318db349c7e0a54c6dc099024462ac8
quieoo <715094648@qq.com>
2022-08-26 16:24:25 +0800

    t

--- lib/doca_argp/doca_argp.c
+++ lib/doca_argp/doca_argp.c
@@ -90,7 +90,9 @@ void doca_argp_start(int argc, char **argv, struct doca_argp_program_general_con
 				int j=0;
 				int flag_length=strlen(p->short_flag);
 				if(flag_length >= 2){
-					printf("			%c %c\n", p->short_flag[1], optarg);
+					printf("			%c\n", p->short_flag[1]);
+					printf("			%c\n", optarg);
+					
 					if(p->short_flag[1] != optarg)
 						{hit=false;	break;}
 					for(j=2;j<flag_length;j++){
