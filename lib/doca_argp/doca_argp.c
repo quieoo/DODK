3a555a360df3763b3fd6452eb4665e6f79540967
quieoo <715094648@qq.com>
2022-08-27 10:50:24 +0800

    t

--- lib/doca_argp/doca_argp.c
+++ lib/doca_argp/doca_argp.c
@@ -37,25 +37,30 @@ void usage(void *config, void *param)
 static void
 set_log_level_callback(void *config, void *param)
 {
-	int level = *(int *) param;
+	int level = *(int *)param;
 	doca_log_global_level_set(level);
 }
 
 /*
 	DOCA_ARGP_TYPE_INT
-	DOCA_ARGP_TYPE_STRING 
+	DOCA_ARGP_TYPE_STRING
 	DOCA_ARGP_TYPE_JSON_OBJ x
 */
-void call_function(struct doca_argp_param *opt, char *param){
-	if(opt->arg_type==DOCA_ARGP_TYPE_INT){
-		int i=0,sum=0;	
-		while (param[i]){
-			sum=10*sum+param[i]-'0';
+void call_function(struct doca_argp_param *opt, char *param)
+{
+	if (opt->arg_type == DOCA_ARGP_TYPE_INT)
+	{
+		int i = 0, sum = 0;
+		while (param[i])
+		{
+			sum = 10 * sum + param[i] - '0';
 			i++;
 		}
 
 		opt->callback(config, &sum);
-	}else if(opt->arg_type==DOCA_ARGP_TYPE_STRING){
+	}
+	else if (opt->arg_type == DOCA_ARGP_TYPE_STRING)
+	{
 		opt->callback(config, param);
 	}
 }
@@ -63,17 +68,19 @@ void call_function(struct doca_argp_param *opt, char *param){
 void doca_argp_start(int argc, char **argv, struct doca_argp_program_general_config **general_config)
 {
 
-	//copy all args
-	int _argc=argc;
-	char* _argv[MAX_PARAM_NUM];
-	for(int i=0;i<argc;i++){
-		char *arg=malloc(100);
+	// copy all args
+	int _argc = argc;
+	char *_argv[MAX_PARAM_NUM];
+	for (int i = 0; i < argc; i++)
+	{
+		char *arg = malloc(100);
 		strcpy(arg, argv[i]);
-		_argv[i]=arg;
+		_argv[i] = arg;
 	}
-	printf("arg: %d\n",argc);
-	for(int i=0;i<argc;i++){
-		printf("	%s\n",argv[i]);
+	printf("arg: %d\n", argc);
+	for (int i = 0; i < argc; i++)
+	{
+		printf("	%s\n", argv[i]);
 	}
 
 	/*
@@ -107,113 +114,77 @@ void doca_argp_start(int argc, char **argv, struct doca_argp_program_general_con
 	doca_argp_register_param(&help);
 
 	// parse doca registered args
-	int n, opt;
-	int opt_idx;
-	static struct option lgopts[MAX_PARAM_NUM];
-	
-	char shortopt[MAX_PARAM_NUM*2];
-	int shortopt_point=0;
-
-	for (int i = 0; i < registered; i++)
+	for (int i = 0; i < argc; i++)
 	{
-		struct doca_argp_param *par = registered_param[i];
-
-		lgopts[i].name = par->long_flag;
-		lgopts[i].val = par->short_flag[0];
-		shortopt[shortopt_point++]=par->short_flag[0];
-		if (par->arg_type != DOCA_ARGP_TYPE_BOOLEAN || strlen(par->short_flag) > 1)
+		struct doca_argp_param *p=NULL;
+		for (int j = 0; j < registered; j++)
 		{
-			lgopts[i].has_arg = true;
-			shortopt[shortopt_point++]=':';
+			// long args
+			if((argv[i][0] == '-' && argv[i][1] == '-' && strcmp(argv[i][2], p->long_flag) == 0) ||
+				(argv[i][0]=='-' && strcmp(argv[i][1], p->short_flag))){ 
+				p=registered_param[j];
+				break;;
+			}
 		}
-	}
-	shortopt[shortopt_point]='\0';
-	while ((opt = getopt_long(argc, argv, shortopt, lgopts, NULL)) != -1)
-	{
-		printf("opt: %c %c\n", opt, *optarg);
-		printf("%s\n", shortopt);
-		bool hit_noce=false;
-		for(int i=0;i<registered;i++){
-			struct doca_argp_param *p=registered_param[i];
-			if(opt == p->short_flag[0]){
-				//for those short flag with more than one characters
-				bool hit=true;
-				int j=0;
-				int flag_length=strlen(p->short_flag);
-				if(flag_length >= 2){
-					if(p->short_flag[1] != *optarg)
-						{hit=false;	break;}
-					for(;j+2<flag_length;j++){
-						if(p->short_flag[j+2] != argv[optind + j])
-							{hit=false; break;}
-					}
-				}			
-				if(hit){
-					printf("hit %s\n", p->short_flag);
-					hit_noce=true;
-					if(p->arg_type == DOCA_ARGP_TYPE_BOOLEAN){
-						bool _param=true;
-						p->callback(config, &(_param));
-					}else{
-						if(flag_length == 1){
-							call_function(p, optarg);
-						}else{
-							call_function(p, argv[optind + j]);
-						}
-					}
-
-					//if a arg hit doca registered list, remove it from _argv
-					int k=0;
-					int num_rm;
-					if(p->arg_type==DOCA_ARGP_TYPE_BOOLEAN)
-						num_rm=1;
-					else
-						num_rm=2;
-					while(k < _argc){
-						bool to_remove=false;
-						if((_argv[k][0]=='-' && _argv[k][1]=='-' && strcmp(_argv[k][2], p->long_flag)==0)
-						|| (_argv[k][0]=='-' && strcmp(_argv[k][1], p->short_flag)==0))
-							to_remove=true;
-						if(to_remove){
-							for(int j=k;j<i+num_rm;j++)
-								free(_argv[k]);
-							for(int j=k;j<_argc-num_rm;j++)
-								_argv[j]=_argv[j+num_rm];
-							_argc-=num_rm;
-						}
-						k++;
-					}
-
-					break;
+		if(p!=NULL){
+			// call_backs
+			if (p->arg_type == DOCA_ARGP_TYPE_BOOLEAN){
+				bool _param = true;
+				p->callback(config, &(_param));
+			}
+			else
+				call_function(p, argv[i+1]);
+			
+			//remove doca regisered args from _argv
+			int k = 0;
+			int num_rm;
+			if (p->arg_type == DOCA_ARGP_TYPE_BOOLEAN)
+				num_rm = 1;
+			else
+				num_rm = 2;
+			while (k < _argc)
+			{
+				bool to_remove = false;
+				if ((_argv[k][0] == '-' && _argv[k][1] == '-' && strcmp(_argv[k][2], p->long_flag) == 0) || 
+				(_argv[k][0] == '-' && strcmp(_argv[k][1], p->short_flag) == 0))
+					to_remove = true;
+				if (to_remove)
+				{
+					for (int j = k; j < i + num_rm; j++)
+						free(_argv[k]);
+					for (int j = k; j < _argc - num_rm; j++)
+						_argv[j] = _argv[j + num_rm];
+					_argc -= num_rm;
 				}
+				k++;
 			}
 		}
 	}
+		printf("_arg: %d\n", _argc);
+		for (int i = 0; i < _argc; i++)
+		{
+			printf("	%s\n", _argv[i]);
+		}
 
-	printf("_arg: %d\n",_argc);
-	for(int i=0;i<_argc;i++){
-		printf("	%s\n",_argv[i]);
-	}
+		int ret = rte_eal_init(_argc, _argv);
 
-
-	int ret = rte_eal_init(_argc, _argv);
-
-	//clean resources
-	for(int i=0;i<argc;i++){
-		free(_argv[i]);
-	}
+		// clean resources
+		for (int i = 0; i < argc; i++)
+		{
+			free(_argv[i]);
+		}
 }
 
-void doca_argp_destroy(void)
-{
-	for (int i = 0; i < registered; i++)
+	void doca_argp_destroy(void)
 	{
-		free(registered_param[i]);
+		for (int i = 0; i < registered; i++)
+		{
+			free(registered_param[i]);
+		}
 	}
-}
 
-void doca_argp_usage(void) {}
+	void doca_argp_usage(void) {}
 
-void doca_argp_register_version_callback(callback_func callback) {}
+	void doca_argp_register_version_callback(callback_func callback) {}
 
-void doca_argp_register_validation_callback(callback_func callback) {}
+	void doca_argp_register_validation_callback(callback_func callback) {}
